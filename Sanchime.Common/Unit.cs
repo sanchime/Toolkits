473d4f13125f7ffe65c5af702a040e9a77dca770
sanchime <61903865+sanchime@users.noreply.github.com>
2024-03-16 15:38:21 +0800

    add Unit type

---
+++ Sanchime.Common/Unit.cs
@@ -0,0 +1,28 @@
+﻿namespace System;
+
+public readonly struct Unit
+{
+    public static Unit Value { get; } = new();
+
+    static Unit() { }
+
+    public override int GetHashCode() => 0;
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+
+        return Equals(this, obj) || (obj is Unit other && Equals(other));
+    }
+
+    public override string ToString() => "()";
+
+    public static bool operator ==(Unit left, Unit right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Unit left, Unit right)
+    {
+        return !(left == right);
+    }
+}
